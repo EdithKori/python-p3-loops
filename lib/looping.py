dac3dc61eaf2d39c70d7f260451684139f3a58b0
Joy Kori <joyedithkori@gmail.com>
2025-11-23 16:34:58 +0300

    finishes loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num = 10  # start at 10
+    while num >= 1:
+        print(num)
+        num -= 1 
+    print("Happy New Year!")
+
+   
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    squared_list = []
+    for int in int_list:
+        squared_list.append(int ** 2)
+    return squared_list
+    
+    
+    
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):  # numbers from 1 to 100
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
